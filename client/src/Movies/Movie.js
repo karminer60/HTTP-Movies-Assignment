189e785cfd703fe5413b327d2b41aebd1ca934fd
karina-rodriguez <karminer60@gmail.com>
2020-06-18 14:48:03 -0700

    editing all modules

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import { Route, NavLink, useHistory } from "react-router-dom";
+import ItemDescription from "./ItemDescription";
+import ItemShipping from "./ItemShipping";
+
+
+  
+
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -33,6 +41,16 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+
+      <button
+        className="md-button"
+        onClick={() => push(`/item-list/update-item/${item.id}`)}
+      >
+        Edit
+      </button>
+      <button className="md-button" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 }
